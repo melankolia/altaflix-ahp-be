a7e46c70b6958691bb2470429a07409ca8bd6072
Ageng Setyo Nugroho <ageng.nugroho@nostratech.com>
2023-08-13 22:09:55 +0700

    Fixing File Sync

--- src/Service/Nilai/index.js
+++ src/Service/Nilai/index.js
@@ -3,10 +3,11 @@ import PenilaianModel from "../../Model/Penilaian/index.js";
 import KriteriaModel from "../../Model/Kriteria/index.js";
 import SubkriteriaModel from "../../Model/Subkriteria/index.js";
 import useFullFunction from "../../Utils/Helper/UsefullFunction.js";
-import ExcelJS from "exceljs"
 import pdf from "pdf-creator-node";
 import path from "path";
 import fs from "fs";
+import Template from "../../Template/index.js"
+
 
 const NilaiService = {
     findAll: async (payload) => {
@@ -158,6 +159,9 @@ const NilaiService = {
             //     weekday: undefined, year: "numeric", month: "long", day: "numeric"
             // })}`;
 
+            const __filename = fileURLToPath(import.meta.url);
+            const __dirname = path.dirname(__filename);
+
             const pathHTML = path.join('src', 'Template', "index.html")
             const pathHTMLDownload = path.join('src', 'Template', 'Report.pdf');
             const bitmap = fs.readFileSync(path.join('src', 'Template', 'altaflix.png'));
@@ -283,10 +287,14 @@ const NilaiService = {
 
             const pathHTML = path.join('src', 'Template', "Report_Individu.html")
             const pathHTMLDownload = path.join('src', 'Template', 'Report_Individu.pdf');
-            const bitmap = fs.readFileSync(path.join('src', 'Template', 'altaflix.png'));
+            const bitmap = fs.readFileSync(path.join(Template.getDirname(), 'altaflix.png'));
+
+            console.log(bitmap);
             const logo = bitmap.toString('base64');
-            
-            const imagePath = Result.image.split("/")
+
+
+
+            const imagePath = Result.image?.split("/")
             const bitmapPhotoKaryawan = fs.readFileSync(path.join('src', 'static-img', 'images', imagePath[imagePath.length - 1]))
             const photoKaryawan = bitmapPhotoKaryawan.toString('base64')
 
