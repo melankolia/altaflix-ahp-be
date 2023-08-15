b4d1a6343ac5e17a85ec2aee6718368ca09ab2a3
Ageng Setyo Nugroho <ageng.nugroho@nostratech.com>
2023-08-16 00:25:39 +0700

    Adjust error

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import { fileURLToPath } from 'url';
 import dotenv from "dotenv";
 import fs from "fs";
 import Router from "./Routes/index.js";
+import Template from './Template/index.js';
+import TemplateImages from "./static-img/images/index.js"
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, ".env") });
-  
+
 const app = express();
 
 // Init Morgan
@@ -26,8 +29,10 @@ app.use(logger("combined"))
 // Manage cors, menentukan situs mana yang boleh akses, situs yang mana yang di blacklist
 app.use(cors());
 
-app.use("/", express.static(path.join("src", "Template")));
-app.use("/", express.static(path.join("src", "static-img")));
+app.use("/", express.static(path.join(Template.getDirname())));
+
+const directory = path.join(TemplateImages.getDirnameImages())
+app.use("/", express.static(path.join(directory)));
 
 
 // Init Body Parser
@@ -36,10 +41,10 @@ app.use(express.urlencoded({ extended: false }));
 
 // Init Cross Server Scripting
 app.use(helmet.xssFilter());
-  
+
 // Init Router
 app.use("/", Router);
-  
+
 app.listen(process.env.SERVER_PORT || 80, () =>
     console.log(`Server Running di Port ${process.env.SERVER_PORT || 80}`)
 );
